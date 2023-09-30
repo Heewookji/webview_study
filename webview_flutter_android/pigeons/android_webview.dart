4317ae3ab16880eb5e21f57f58a6a7e120e05c8e
heewookji <jhu2269@gmail.com>
2023-09-30 20:28:44 +0900

    1. 실제로 Native와의 통신을 담당할 CustomViewCallbackHostApi, CustomViewCallbackFlutterApi 정의 2. pigeon을 이용해 생성

--- webview_flutter_android/pigeons/android_webview.dart
+++ webview_flutter_android/pigeons/android_webview.dart
@@ -446,3 +446,13 @@ abstract class GeolocationPermissionsCallbackFlutterApi {
   /// Create a new Dart instance and add it to the `InstanceManager`.
   void create(int instanceId);
 }
+
+@HostApi(dartHostTestHandler: 'TestCustomViewCallbackHostApi')
+abstract class CustomViewCallbackHostApi {
+  void onCustomViewHidden(int identifier);
+}
+
+@FlutterApi()
+abstract class CustomViewCallbackFlutterApi {
+  void create(int identifier);
+}

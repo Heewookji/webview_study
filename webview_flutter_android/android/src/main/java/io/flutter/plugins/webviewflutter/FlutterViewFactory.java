2b087bd4dfeab19a9823bd007c5201d9cfcc2b1d
heewookji <jhu2269@gmail.com>
2023-10-01 21:42:29 +0900

    PlatformViewFactory의 create에서 View가 들어올 경우 PlatformView로 반환

--- webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterViewFactory.java
+++ webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterViewFactory.java
@@ -5,6 +5,8 @@
 package io.flutter.plugins.webviewflutter;
 
 import android.content.Context;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import io.flutter.plugin.common.StandardMessageCodec;
@@ -27,10 +29,24 @@ class FlutterViewFactory extends PlatformViewFactory {
       throw new IllegalStateException("An identifier is required to retrieve WebView instance.");
     }
 
-    final PlatformView view = instanceManager.getInstance(identifier);
-    if (view == null) {
-      throw new IllegalStateException("Unable to find View instance: " + args);
+    final Object instance = instanceManager.getInstance(identifier);
+
+    if (instance instanceof PlatformView) {
+      return (PlatformView) instance;
+    } else if (instance instanceof View) {
+      return new PlatformView() {
+        @Override
+        public View getView() {
+          return (View) instance;
+        }
+
+        @Override
+        public void dispose() {
+
+        }
+      };
     }
-    return view;
+
+    throw new IllegalStateException(String.format("Unable to find a PlatformView or View instance: args: %s, instance: %s", args, instance));
   }
 }
